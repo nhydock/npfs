e036ac34a61b5786a899ecea3e7059c817fcc17f
Nicholas Hydock <nhydock@gmail.com>
2014-12-10 17:35:23 -0500

    Add deploy script and move idl file into its own folder

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+scp -i Ship.pem dist/server.jar p1078@dfsVirginia:server.jar
+scp -i Ship.pem dist/server.jar p1078@dfsIreland:server.jar
+scp -i Ship.pem dist/server.jar p1078@dfsJapan:server.jar
+
+scp -i Ship.pem dist/client.jar p1078@clientTwo:client.jar
+scp -i Ship.pem dist/client.jar p1078@clientThree:client.jar
+scp -i Ship.pem dist/client.jar p1078@clientFour:client.jar
